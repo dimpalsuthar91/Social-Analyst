f01e625fe9964c2cee59591c3f28e0cc8e459167
Dimpal Gajjar <dimpalsuthar91@gmail.com>
2018-09-27 13:05:57 -0400

    test

---
+++ config.py
@@ -0,0 +1,6 @@
+# These keys are not valid. Use your own API keys
+
+consumer_key = "38tfo1RAdqMA7QqtZkl2p8iFg"
+consumer_secret = "mw1MTXr1m0anLjTOJdNcDzrXOGfpIknpDvq0DEfqpExfY46Bg5"
+access_token = "1007991814924701697-Uk0OUKwnvayJBxR4JNUM3emGBe0KGh"
+access_token_secret = "2iCyVGjqCb3QafrjoEn05SauJV52ZbtPh3Hp24ndN2HVQ"
